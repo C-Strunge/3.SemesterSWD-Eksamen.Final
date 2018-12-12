a4a96b7de459f53b0a4f8a1623802135273b270e
MatthiasSkou <ms@klinikskou.dk>
2018-12-12 15:07:02 +0100

    US03.1 done minus session

--- TheNext/src/main/java/mandatory/two/controller/OfferController.java
+++ TheNext/src/main/java/mandatory/two/controller/OfferController.java
@@ -1,7 +1,13 @@
 package mandatory.two.controller;
 
+import mandatory.two.model.Offer;
+import mandatory.two.repository.OfferRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * Created by Matthias Skou 12/12/2018
@@ -9,8 +15,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OfferController {
+    @Autowired
+    OfferRepository offerRepo;
 
 
-@GetMapping("/")
+    @GetMapping("/offer/create")
+    public String createOffer(Model model) {
+
+        model.addAttribute("offer", new Offer());
+
+        return "Offer/createOffer";
+    }
+
+    @PostMapping("/offer/create")
+    public String createOffer(@ModelAttribute Offer offer) {
+
+        offerRepo.save(offer);
+
+        return "";
+    }
 
 }
